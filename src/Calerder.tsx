4106889d6ef9bf38747c2c67831503b73e4ccb33
priya_n2022 <glenraise@gmail.com>
2023-09-16 17:37:39 +0530

    updates

--- src/Calerder.tsx
+++ src/Calerder.tsx
@@ -1,26 +1,26 @@
 import React from 'react'
-
+import './Calendar.css'
 interface Events {
-    title:string,
-    startDate:Date
+    title: string,
+    startDate: Date
 }
 
-const events:Events[]=[
+const events: Events[] = [
     {
-        title:"Meeting 1",
-        startDate:new Date(2023, 8, 12),
+        title: "Meeting 1",
+        startDate: new Date(2023, 8, 12),
     },
     {
-        title:"Meeting 2",
+        title: "Meeting 2",
         startDate: new Date(2023, 8, 14)
     }
 ]
-    const daysInMonth =(year:number,month:number)=>{
-        //For Getting a last Date of a month
-        return new Date(year, month + 1, 0).getDate()
-    }
+const daysInMonth = (year: number, month: number) => {
+    //For Getting a last Date of a month
+    return new Date(year, month + 1, 0).getDate()
+}
 
-const Calerder:React.FC = () => {
+const Calerder: React.FC = () => {
     const currentDate = new Date()
     const year = currentDate.getFullYear()
     const month = currentDate.getMonth()
@@ -28,27 +28,26 @@ const Calerder:React.FC = () => {
     const numOfDaysInMonth = daysInMonth(year, month)
 
 
-    const startDay =  new Date(year,month, 1).getDay()
+    const startDay = new Date(year, month, 1).getDay()
 
     //Creating a calendar and push my Data 
 
-    const calendarDays =[]
+    const calendarDays = []
 
     //looping through untill the endDate
 
-    for(let i = 1; i<= numOfDaysInMonth;i++)
-    {
-        const date = new Date(year,month,i)
-        const dayEvents = events.filter((event)=>{
-            isSameDay(event.startDate,date)
+    for (let i = 1; i <= numOfDaysInMonth; i++) {
+        const date = new Date(year, month, i)
+        const dayEvents = events.filter((event) => {
+            isSameDay(event.startDate, date)
         })
 
         calendarDays.push(
-            <div key={i} className="calendar-day"> 
+            <div key={i} className="calendar-day">
                 <span className="day-number">{i}</span>
                 {
-                    dayEvents.map((event, index)=>(
-                        <div key={index} className ="event">
+                    dayEvents.map((event, index) => (
+                        <div key={index} className="event">
                             {event.title}
                         </div>
                     )
@@ -59,14 +58,49 @@ const Calerder:React.FC = () => {
     }
 
     return (
-        <div>
-            
+        <div className="Calendar">
+
+            {/* Current Date and year , and current month */}
+            <h1>{currentDate.toLocaleString('default', { month: 'long' })} {year}</h1>
+            {/* Calendar-weeks  */}
+            <div className="calendar-header">
+                <div>Sun</div>
+                <div>Mon</div>
+                <div>Tue</div>
+                <div>Wed</div>
+                <div>Thur</div>
+                <div>Fri</div>
+                <div>Sat</div>
+            </div>
+            {/* Array for the days  */}
+            <div className="calendar-grid">
+
+                {
+                    [...Array(startDay).fill(null), ...calendarDays].map((day, index) => 
+                    {
+                        const date = new Date(year, month, index + 1);
+                        const dayEvents = events.filter((event) => isSameDay(event.startDate, date));
+
+                        return (
+                            <div key={index} className="calendar-day">
+                                <span className="day-number">{index + 1}</span>
+                                {dayEvents.map((event, eventIndex) => (
+                                    <div key={eventIndex} className="event">
+                                        {event.title}
+                                    </div>
+                                ))}
+                            </div>
+                        );
+                    }
+                    )
+                }
+            </div>
         </div>
     )
 }
-const isSameDay =(date1:Date ,date2:Date)=>{
+const isSameDay = (date1: Date, date2: Date) => {
 
-    return(
+    return (
         date1.getFullYear() === date2.getFullYear() &&
         date1.getMonth() === date2.getMonth() &&
         date1.getDate() === date2.getDate()
