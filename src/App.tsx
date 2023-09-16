58480d5da0854096da1637ddbebd393fed31dde5
priya_n2022 <glenraise@gmail.com>
2023-09-16 16:51:53 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import WeekWise from './WeekWise'
+import Priya from './priya'
 
 const App = () => {
   return (
     <div>
       <WeekWise />
+      <Priya />
     </div>
   )
 }
